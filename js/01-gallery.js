957d7341a9c375d232044b9666e3b703622b35f1
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-13 16:51:48 +0200

    Finish Home Work 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const GALLERY_WRAPPER = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const gallery = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-original="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+GALLERY_WRAPPER.innerHTML = gallery;
+
+let instance;
+
+const openWindow = (event) => {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  instance = basicLightbox.create(`
+    <img src=${event.target.dataset.original} style="margin: auto; position: fixed; top:50%; left:50%; transform: translate(-50%, -50%); max-width: 95vw; max-height: 95vh ">`);
+  instance.show();
+};
+
+const closeWindow = (event) => {
+  if (instance.visible() === true) {
+    if (event.key === "Escape") {
+      instance.close(console.log("closed"));
+    }
+  }
+};
+
+GALLERY_WRAPPER.addEventListener("click", openWindow);
+document.addEventListener("keydown", closeWindow);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const GALLERY_WRAPPER = document.querySelector(".gallery");
+console.log(GALLERY_WRAPPER);
+const gallery = galleryItems
+  .map(
+    (item) =>
+      `<ul class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-original="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </ul>`
+  )
+  .join("");
 
-console.log(galleryItems);
+GALLERY_WRAPPER.innerHTML = gallery;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
